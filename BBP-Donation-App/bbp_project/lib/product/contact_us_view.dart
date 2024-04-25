9898e97ecce76e5c66ab1038c1cdc8319ee226f1
UycGitHub <umut.yusuf.cinar.mail@gmail.com>
2024-04-25 21:03:22 +0300

    Tasarlama: contact_us_view.dart tasarlandı.

--- BBP-Donation-App/bbp_project/lib/product/contact_us_view.dart
+++ BBP-Donation-App/bbp_project/lib/product/contact_us_view.dart
@@ -3,6 +3,7 @@
 /// ve iletişim bilgilerimizi kullancıya sunmak için oluşturuldu.
 
 import 'package:bbp_project/product/custom_app_bar.dart';
+import 'package:bbp_project/product/language/languages.dart';
 import 'package:flutter/material.dart';
 
 class ContactUsView extends StatelessWidget {
@@ -10,8 +11,34 @@ class ContactUsView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       appBar: CustomAppBar(),
+      body: ListView(
+        children: [
+          Center(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(vertical: 25),
+              child: SizedBox(
+                child: Text(
+                  LanguageItems().contactUsviewMainText,
+                  style: const TextStyle(fontSize: 25),
+                ),
+              ),
+            ),
+          ),
+          ListTile(
+            leading: const Icon(Icons.email),
+            title: Text(LanguageItems().contactUsViewMailListTileTitle),
+            subtitle: Text(LanguageItems().contactUsViewMailListTileSubtitle),
+          ),
+          ListTile(
+            leading: const Icon(Icons.phone),
+            title: Text(LanguageItems().contactUsViewPhoneNumberListTileTitle),
+            subtitle:
+                Text(LanguageItems().contactUsViewPhoneNumberListTileSubtitle),
+          ),
+        ],
+      ),
     );
   }
 }
