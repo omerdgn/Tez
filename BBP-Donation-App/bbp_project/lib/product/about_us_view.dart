c6845f574872eaa6f58bde81ddf23849bd1e0808
UycGitHub <umut.yusuf.cinar.mail@gmail.com>
2024-04-28 14:41:21 +0300

    Tasarlama: about_us_view.dart tasarlandı.

--- BBP-Donation-App/bbp_project/lib/product/about_us_view.dart
+++ BBP-Donation-App/bbp_project/lib/product/about_us_view.dart
@@ -1,8 +1,9 @@
-/// Bu class home_page_view.dart ekranındaki Biz Kimiz isimli butona tıklandığında
-/// Biz Kimiz sayfasını ekrana çizdirmek
-/// ve projemiz hakkımızda bilgi vermek için oluşturuldu.
+///Bu class home_page_view.dart ekranındaki Biz Kimiz isimli butona tıklandığında
+///Biz Kimiz sayfasını ekrana çizdirmek
+///ve projemiz hakkımızda bilgi vermek için oluşturuldu.
 
 import 'package:bbp_project/product/custom_app_bar.dart';
+import 'package:bbp_project/product/language/languages.dart';
 import 'package:flutter/material.dart';
 
 class AboutUsView extends StatelessWidget {
@@ -10,8 +11,31 @@ class AboutUsView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      appBar: CustomAppBar(),
+    return Scaffold(
+      appBar: const CustomAppBar(),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              LanguageItems().aboutUsViewWhoWeAreTitle,
+              style: const TextStyle(
+                fontSize: 25.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 10),
+            Text(
+              LanguageItems().aboutUsViewWhoWeAreText,
+              style: const TextStyle(
+                fontSize: 16.0,
+              ),
+            ),
+            const SizedBox(height: 10),
+          ],
+        ),
+      ),
     );
   }
 }
